df67c716f7717588ac359b517169aff0219436ac
Vitaliy Dovzhenko <dovzhenko.vit@gmail.com>
2025-04-09 09:53:27 +0300

    [feat] add rest API for book

--- library/book/views.py
+++ library/book/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Book
 from .forms import AddBookForm, UpdateBookForm
+from .serializers import BookSerializer
 
 
 class BookListView(ListView):
@@ -60,3 +64,59 @@ class UserBookListView(ListView):
     def get_queryset(self):
         user_id = self.kwargs['user_id']
         return Book.objects.filter(user__id=user_id)
+
+
+class BookAPIListView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.data.get('name'),
+            'author': request.data.get('author'),
+            'description': request.data.get('description'),
+            'count': request.data.get('count'),
+            'publication_year': request.data.get('publication_year'),
+            'issue_date': request.data.get('issue_date'),
+        }
+        serializer = BookSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookAPIDetailView(APIView):
+    def get(self, request, id=None):
+        try:
+            book = Book.objects.get(pk=id)
+            serializer = BookSerializer(book)
+        except Book.DoesNotExist:
+            return Response({'status': f'Book with id {id} not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id=None):
+        if id is None:
+            return Response({'status': 'id is required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            book = Book.objects.get(pk=id)
+            book.delete()
+        except Book.DoesNotExist:
+            return Response({'status': f'Book with id {id} not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id=None):
+        if id is None:
+            return Response({'status': 'id is required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            book = Book.objects.get(pk=id)
+        except Book.DoesNotExist:
+            return Response({'status': f'Book with id {id} not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = BookSerializer(book, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
